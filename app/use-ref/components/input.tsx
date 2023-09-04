815d07aed4b72921e456d9a9b3cf6f414b6bec82
sri ghantasala <srini.ghantasala31@gmail.com>
2023-09-05 01:06:57 +0530

    fixed build errors

--- app/use-ref/components/input.tsx
+++ app/use-ref/components/input.tsx
@@ -1,16 +1,20 @@
 import { FC, memo } from "react";
 
-
-  type Props = {
-    name: string;
-    changeHandler:(name:string)=>void
-  }
- const  Input:FC<Props>=memo(({name,changeHandler})=>{
-
-    console.log('Input cmp rendered')
-  return <>
-  
-  <input  type ="text" onChange={e=> changeHandler(e.target.value)} value={name}/>
-  </>
-})
+type Props = {
+  name: string;
+  changeHandler: (name: string) => void;
+};
+const Input: FC<Props> = memo(({ name, changeHandler }) => {
+  console.log("Input cmp rendered");
+  return (
+    <>
+      <input
+        type="text"
+        name="test"
+        onChange={(e) => changeHandler(e.target.value)}
+        value={name}
+      />
+    </>
+  );
+});
 export default Input;
