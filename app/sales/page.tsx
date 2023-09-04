1c807385495352cbdf2eff9b5243deb5f7d7352a
sri ghantasala <srini.ghantasala31@gmail.com>
2023-09-05 01:05:04 +0530

    fixed build errors

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,30 +1,32 @@
+const getSales: () => Promise<Sale[]> = async () => {
+  try {
+    const resp = await fetch("http://localhost:8080/sales");
 
-const getSales:()=> Promise<Sale[]> = async()=>{
-    try{
-        const resp= await fetch('http://localhost:8080/sales') ;
-
-        return resp.json()
-    }
-    catch(error){
-    console.log("err=>>>", error)
-    }
-
-}
+    return resp.json();
+  } catch (error) {
+    console.log("err=>>>", error);
+  }
+};
 type Sale = {
-    saleDate:Date;
-    storeLocation:string;
-    customer:{
-        email: string
-    },
-    purchaseMethod: string;
-}
-export default async function  SalesPage(){
+  saleDate: Date;
+  storeLocation: string;
+  customer: {
+    email: string;
+  };
+  purchaseMethod: string;
+};
+export default async function SalesPage() {
   const sales = await getSales();
-  console.log("sales->>>>",sales)
-return <>
-  <ul>
-    {sales.map(sale => <li>{sale.purchaseMethod}  {sale.customer.email} {sale.storeLocation}</li>)}
-  </ul>
-</>
-
+  console.log("sales->>>>", sales);
+  return (
+    <>
+      <ul>
+        {sales.map((sale, i) => (
+          <li key={i}>
+            {sale.purchaseMethod} {sale.customer.email} {sale.storeLocation}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- app/suspended-component/page.tsx
+++ app/suspended-component/page.tsx
@@ -1,26 +1,28 @@
-  
-  import Link from 'next/link';
-import 'react-loading-skeleton/dist/skeleton.css';
-import { Employee, Employees } from './Employees';
-  
+import Link from "next/link";
+import "react-loading-skeleton/dist/skeleton.css";
+import { Employee, Employees } from "./Employees";
 
-  
-async function getEmployees():Promise<Employee[]>{
-  return new Promise((resolve,reject)=>{
-   setTimeout(()=>{
-    resolve(Employees)
-   },2000)
-  })
+async function getEmployees(): Promise<Employee[]> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(Employees);
+    }, 2000);
+  });
 }
 
-export default async function SuspendPage(){
-  
-    const employees = await getEmployees();
+export default async function SuspendPage() {
+  const employees = await getEmployees();
 
-    return(<>
-     <h1>delayed list</h1>
-       <ul>
-        {employees.map(emp=> (<li><Link href={`/suspended-component/${emp.id}`}>{emp.name}</Link></li>))}
-       </ul>
-    </>)
+  return (
+    <>
+      <h1>delayed list</h1>
+      <ul>
+        {employees.map((emp, i) => (
+          <li key={i}>
+            <Link href={`/suspended-component/${emp.id}`}>{emp.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
